9da12be1697bbf34ecb805400f50d83496ae2f3b
Anurag Dutta <imanurag18@gmail.com>
2022-10-29 00:53:38 +0530

    Create Clock.jsx
    Created a clock using React

---
+++ Clock.jsx
@@ -0,0 +1,13 @@
+import React from 'react'  //rafce
+
+let t = new Date().toLocaleTimeString();
+const App = () => {
+  return (
+    <>
+    <h1>{t}</h1>
+    
+    </>
+  );
+};
+
+export default App
