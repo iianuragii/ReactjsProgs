1de5053297fdfaeacdc472c148649c8df22e2bae
Anurag Dutta <imanurag18@gmail.com>
2022-10-29 20:56:25 +0530

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+// import Resturant from "./component/Basics/Resturant";
+ import UseState from "./Hooks/useState";
+// import UseEffect from "./component/Hooks/useEffect";
+//import UseReducer from "./component/Hooks/useReducer";
+
+const App = () => {
+  return <UseState />;
+};
+
+export default App;
